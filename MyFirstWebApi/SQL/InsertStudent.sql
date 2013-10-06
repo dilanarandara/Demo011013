052cb1fdf5333b0116cf10d04fcd8b6d760140d2
Dilan Arandara <dilan.madu@gmail.com>
2013-10-07 00:18:03 +0530

    SQL query file for Insert Students.

---
+++ MyFirstWebApi/SQL/InsertStudent.sql
@@ -0,0 +1,16 @@
+INSERT INTO [StudentDemo].[dbo].[Students]
+           ([FirstName], [LastName], [Age])
+     VALUES
+           ('Dilan', 'Arandara', 25),
+           ('Enrique', 'Iglesias', 38),
+           ('Bruno', 'Mars', 27),
+           ('Justin', 'Timberlake', 32),
+           ('Taylor', 'Swift', 23),
+           ('Selena', 'Gomez', 21),
+           ('Katy', 'Perry', 28),
+           ('David', 'Guetta', 45),
+           ('Chris', 'Brown', 24),
+           ('Taio', 'Cruz', 28),
+           ('Ne', 'Yo', 30),
+           ('Jason', 'Derulo', 24)
+GO
