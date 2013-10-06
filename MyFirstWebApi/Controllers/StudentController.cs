75fddb25f946514a4617c5dbd23083258e9f4736
Dilan Arandara <dilan.madu@gmail.com>
2013-10-07 00:42:05 +0530

    Add OData qury supportability to StudentController.

--- MyFirstWebApi/Controllers/StudentController.cs
+++ MyFirstWebApi/Controllers/StudentController.cs
@@ -12,100 +12,101 @@ using MyFirstWebApi.Models;
 
 namespace MyFirstWebApi.Controllers
 {
-    public class StudentController : ApiController
+  public class StudentController : ApiController
+  {
+    private StudentContext db = new StudentContext();
+
+    // GET api/Student
+    [Queryable]
+    public IQueryable<Student> GetStudents()
     {
-        private StudentContext db = new StudentContext();
-
-        // GET api/Student
-        public IEnumerable<Student> GetStudents()
-        {
-            return db.Students.AsEnumerable();
-        }
-
-        // GET api/Student/5
-        public Student GetStudent(int id)
-        {
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
-            }
-
-            return student;
-        }
-
-        // PUT api/Student/5
-        public HttpResponseMessage PutStudent(int id, Student student)
-        {
-            if (!ModelState.IsValid)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-            }
-
-            if (id != student.StudentId)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
-            }
-
-            db.Entry(student).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
-
-            return Request.CreateResponse(HttpStatusCode.OK);
-        }
-
-        // POST api/Student
-        public HttpResponseMessage PostStudent(Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Students.Add(student);
-                db.SaveChanges();
-
-                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, student);
-                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = student.StudentId }));
-                return response;
-            }
-            else
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-            }
-        }
-
-        // DELETE api/Student/5
-        public HttpResponseMessage DeleteStudent(int id)
-        {
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound);
-            }
-
-            db.Students.Remove(student);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
-
-            return Request.CreateResponse(HttpStatusCode.OK, student);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
-        }
+      return db.Students.AsQueryable();
     }
+
+    // GET api/Student/5
+    public Student GetStudent(int id)
+    {
+      Student student = db.Students.Find(id);
+      if (student == null)
+      {
+        throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+      }
+
+      return student;
+    }
+
+    // PUT api/Student/5
+    public HttpResponseMessage PutStudent(int id, Student student)
+    {
+      if (!ModelState.IsValid)
+      {
+        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+      }
+
+      if (id != student.StudentId)
+      {
+        return Request.CreateResponse(HttpStatusCode.BadRequest);
+      }
+
+      db.Entry(student).State = EntityState.Modified;
+
+      try
+      {
+        db.SaveChanges();
+      }
+      catch (DbUpdateConcurrencyException ex)
+      {
+        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+      }
+
+      return Request.CreateResponse(HttpStatusCode.OK);
+    }
+
+    // POST api/Student
+    public HttpResponseMessage PostStudent(Student student)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Students.Add(student);
+        db.SaveChanges();
+
+        HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, student);
+        response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = student.StudentId }));
+        return response;
+      }
+      else
+      {
+        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+      }
+    }
+
+    // DELETE api/Student/5
+    public HttpResponseMessage DeleteStudent(int id)
+    {
+      Student student = db.Students.Find(id);
+      if (student == null)
+      {
+        return Request.CreateResponse(HttpStatusCode.NotFound);
+      }
+
+      db.Students.Remove(student);
+
+      try
+      {
+        db.SaveChanges();
+      }
+      catch (DbUpdateConcurrencyException ex)
+      {
+        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+      }
+
+      return Request.CreateResponse(HttpStatusCode.OK, student);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      db.Dispose();
+      base.Dispose(disposing);
+    }
+  }
 }
